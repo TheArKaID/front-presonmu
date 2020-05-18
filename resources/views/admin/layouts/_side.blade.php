52291b2f2f9fd2f794abc978287dc7a156a15827
Arifia Kasastra R <arka.progammer@gmail.com>
2020-05-19 03:28:10 +0700

    Make active menu showed in sidebar

--- resources/views/admin/layouts/_side.blade.php
+++ resources/views/admin/layouts/_side.blade.php
@@ -9,10 +9,10 @@
             <nav class="sidebar-nav left-sidebar-menu-pro">
                 <ul class="metismenu" id="menu1">
                     <li>
-                        <a title="Landing Page" href="/dashboard" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Dashboard</span></a>
+                        <a title="Landing Page" class="{{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Dashboard</span></a>
                     </li>
                     <li>
-                        <a title="Landing Page" href="/dashboard/pendaftar" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Pendaftar</span></a>
+                        <a title="Landing Page" class="{{ Request::is('dashboard/pendaftar') ? 'active' : '' }}" href="/dashboard/pendaftar" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Pendaftar</span></a>
                     </li>
                     <li>
                         <a class="has-arrow" href="#">
@@ -20,11 +20,11 @@
                             <span class="mini-click-non">Setting</span>
                         </a>
                         <ul class="submenu-angle" aria-expanded="true">
-                            <li><a title="Tahun" href="/dashboard/tahun"><span class="mini-sub-pro">Tahun</span></a></li>
-                            <li><a title="Tentang" href="/dashboard/tentang"><span class="mini-sub-pro">Tentang</span></a></li>
-                            <li><a title="Kegiatan" href="/dashboard/kegiatan"><span class="mini-sub-pro">Kegiatan</span></a></li>
-                            <li><a title="Alur" href="/dashboard/alur"><span class="mini-sub-pro">Alur</span></a></li>
-                            <li><a title="Kesan" href="/dashboard/kesan"><span class="mini-sub-pro">Kesan</span></a></li>
+                            <li><a title="Tahun" class="{{ Request::is('dashboard/tahun') ? 'active' : '' }}" href="/dashboard/tahun"><span class="mini-sub-pro">Tahun</span></a></li>
+                            <li><a title="Tentang" class="{{ Request::is('dashboard/tentang') ? 'active' : '' }}" href="/dashboard/tentang"><span class="mini-sub-pro">Tentang</span></a></li>
+                            <li><a title="Kegiatan" class="{{ Request::is('dashboard/kegiatan') ? 'active' : '' }}" href="/dashboard/kegiatan"><span class="mini-sub-pro">Kegiatan</span></a></li>
+                            <li><a title="Alur" class="{{ Request::is('dashboard/alur') ? 'active' : '' }}" href="/dashboard/alur"><span class="mini-sub-pro">Alur</span></a></li>
+                            <li><a title="Kesan" class="{{ Request::is('dashboard/kesan') ? 'active' : '' }}" href="/dashboard/kesan"><span class="mini-sub-pro">Kesan</span></a></li>
                         </ul>
                     </li>
                 </ul>
