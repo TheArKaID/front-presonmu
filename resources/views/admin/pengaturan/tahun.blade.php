fdb1ad067994781fa6a27e42e78ba8614705075d
Arifia Kasastra R <arka.progammer@gmail.com>
2020-05-19 00:43:33 +0700

    Update Admin View of Tahun

--- resources/views/admin/pengaturan/tahun.blade.php
+++ resources/views/admin/pengaturan/tahun.blade.php
@@ -9,14 +9,14 @@
 <div class="analytics-sparkle-area">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="sparkline10-list mt-b-30">
-                    <div class="sparkline10-hd">
-                        <div class="main-sparkline10-hd">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="sparkline12-list mg-b-30 mg-t-30">
+                    <div class="sparkline12-hd">
+                        <div class="main-sparkline12-hd">
                             <h1><span class="basic-ds-n">Tambah Tahun Ajaran</span></h1>
                         </div>
                     </div>
-                    <div class="sparkline10-graph">
+                    <div class="sparkline12-graph">
                         <div class="basic-login-form-ad">
                             <div class="row">
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -28,13 +28,13 @@
                                         {!! Form::open(['url' => '/dashboard/tahun/tambah']) !!}
                                             <div class="form-group-inner">
                                                 <div class="row">
-                                                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
                                                         {!! Form::number('tahunmulai', '', ['placeholder' => 'Tahun Mulai', 'maxlength' => '4', 'class' => 'form-control basic-ele-mg-b-10 responsive-mg-b-10', 'value' => '']) !!}
                                                     </div>
-                                                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
                                                         {!! Form::number('tahunselesai', '', ['placeholder' => 'Tahun Selesai', 'maxlength' => '4', 'class' => 'form-control basic-ele-mg-b-10 responsive-mg-b-10', 'value' => '']) !!}
                                                     </div>
-                                                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                                    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
                                                         <div class="login-btn-inner">
                                                             <div class="row">
                                                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -55,10 +55,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="sparkline10-list mt-b-30">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="sparkline10-list mg-b-30 mg-t-30">
                     <div class="sparkline10-hd">
                         <div class="main-sparkline10-hd">
                             <h1><span class="basic-ds-n">Pilih Tahun Aktif</span></h1>
@@ -68,12 +66,12 @@
                         <div class="basic-login-form-ad">
                             <div class="row">
                                 {!! Form::open(['url' => '/dashboard/tahun/simpan']) !!}
-                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
                                     <div class="form-select-list">
                                         {!! Form::select('tahun', [$tahun], $tahunaktif, ['class' => 'form-control custom-select-value']) !!}
                                     </div>
                                 </div>
-                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
                                     <div class="login-btn-inner">
                                         <div class="row">
                                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
